27b5604fefc2ebc0093c0a619a7e412cd89ed59f
anatolij1923 <kostyahaustow@gmail.com>
2025-06-09 19:38:39 +0300

    transparency in theme

--- nvim/.config/nvim/lua/plugins/colorsheme.lua
+++ nvim/.config/nvim/lua/plugins/colorsheme.lua
@@ -1,10 +1,12 @@
 return {
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd("colorscheme onedark_vivid")
-    end,
-  },
+	{
+		"olimorris/onedarkpro.nvim",
+		priority = 1000,
+		config = function()
+			require("onedarkpro").setup({
+                options = { transparency = true }
+			})
+			vim.cmd("colorscheme onedark_vivid")
+		end,
+	},
 }
-
