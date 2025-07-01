acc9a528928144f8f28c6c7981f4dffac3896ca8
anatolij1923 <kostyahaustow@gmail.com>
2025-07-01 23:51:44 +0300

    fix: added hide function in launch

--- shell/widget/applauncher/Applauncher.tsx
+++ shell/widget/applauncher/Applauncher.tsx
@@ -1,14 +1,13 @@
 import { For, createState } from "ags";
 import { Astal, Gtk, Gdk } from "ags/gtk4";
 import AstalApps from "gi://AstalApps";
-import Graphene from "gi://Graphene";
+import Window from "../common/Window";
+import App from "ags/gtk4/app";
 
 const { TOP, BOTTOM, LEFT, RIGHT } = Astal.WindowAnchor;
 
 export default function Applauncher() {
-  let contentbox: Gtk.Box;
   let searchentry: Gtk.Entry;
-  let win: Astal.Window;
 
   const apps = new AstalApps.Apps();
   const [list, setList] = createState(new Array<AstalApps.Application>());
@@ -20,93 +19,71 @@ export default function Applauncher() {
 
   function launch(app?: AstalApps.Application) {
     if (app) {
-      win.hide();
+      // The common Window component will handle hiding the window
       app.launch();
+      hide();
     }
   }
 
-  // close on ESC
+  function hide() {
+    App.get_window("launcher")?.hide();
+  }
+
   // handle alt + number key
   function onKey(
     _e: Gtk.EventControllerKey,
     keyval: number,
     _: number,
-    mod: number,
+    mod: number
   ) {
-    if (keyval === Gdk.KEY_Escape) {
-      win.visible = false;
-      return;
-    }
-
     if (mod === Gdk.ModifierType.ALT_MASK) {
       for (const i of [1, 2, 3, 4, 5, 6, 7, 8, 9] as const) {
         if (keyval === Gdk[`KEY_${i}`]) {
-          return launch(list.get()[i - 1]);
+          return launch(list.get()[i - 1]), hide();
         }
       }
     }
   }
 
-  // close on clickaway
-  function onClick(_e: Gtk.GestureClick, _: number, x: number, y: number) {
-    const [, rect] = contentbox.compute_bounds(win);
-    const position = new Graphene.Point({ x, y });
-
-    if (!rect.contains_point(position)) {
-      win.visible = false;
-      return true;
-    }
-  }
-
   return (
-    <window
-      $={(ref) => (win = ref)}
+    <Window
       name="launcher"
       anchor={TOP | BOTTOM | LEFT | RIGHT}
       exclusivity={Astal.Exclusivity.IGNORE}
       keymode={Astal.Keymode.EXCLUSIVE}
-      onNotifyVisible={({ visible }) => {
+      onKey={onKey}
+      onVisibilityChange={(visible) => {
         if (visible) searchentry.grab_focus();
         else searchentry.set_text("");
       }}
     >
-      <Gtk.EventControllerKey onKeyPressed={onKey} />
-      <Gtk.GestureClick onPressed={onClick} />
-      <box
-        $={(ref) => (contentbox = ref)}
-        name="launcher-content"
-        valign={Gtk.Align.CENTER}
-        halign={Gtk.Align.CENTER}
-        orientation={Gtk.Orientation.VERTICAL}
-      >
-        <box class="search">
-          <entry
-            $={(ref) => (searchentry = ref)}
-            hexpand
-            onNotifyText={({ text }) => search(text)}
-            primaryIconName={"system-search-symbolic"}
-            placeholderText="Search"
-          />
-        </box>
-        <Gtk.Separator visible={list((l) => l.length > 0)} class="separator" />
-        <box orientation={Gtk.Orientation.VERTICAL}>
-          <For each={list}>
-            {(app, index) => (
-              <button onClicked={() => launch(app)}>
-                <box spacing={12}>
-                  <image iconName={app.iconName} pixelSize={32} />
-                  <label label={app.name} maxWidthChars={40} wrap />
-                  <label
-                    hexpand
-                    halign={Gtk.Align.END}
-                    label={index((i) => `󰘳  ${i + 1}`)}
-                  />
-                </box>
-              </button>
-            )}
-          </For>
-        </box>
+      <box class="search">
+        <entry
+          $={(ref) => (searchentry = ref)}
+          hexpand
+          onNotifyText={({ text }) => search(text)}
+          primaryIconName={"system-search-symbolic"}
+          placeholderText="Search"
+        />
       </box>
-    </window>
+      <Gtk.Separator visible={list((l) => l.length > 0)} class="separator" />
+      <box orientation={Gtk.Orientation.VERTICAL}>
+        <For each={list}>
+          {(app, index) => (
+            <button onClicked={() => launch(app)}>
+              <box spacing={12}>
+                <image iconName={app.iconName} pixelSize={32} />
+                <label label={app.name} maxWidthChars={40} wrap />
+                <label
+                  hexpand
+                  halign={Gtk.Align.END}
+                  label={index((i) => `󰘳  ${i + 1}`)}
+                />
+              </box>
+            </button>
+          )}
+        </For>
+      </box>
+    </Window>
   );
 }
