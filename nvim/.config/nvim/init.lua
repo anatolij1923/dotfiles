76d67c82811f00f7a3df79cb7fadd0c0e80d3552
anatolij1923 <kostyahaustow@gmail.com>
2025-07-02 17:13:12 +0300

    nvim: added some themes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,4 @@
 require("core.options")
 require("core.keymaps")
 require("lazy_setup")
-
+require("current-theme")
--- nvim/.config/nvim/lua/plugins/colorsheme.lua
+++ nvim/.config/nvim/lua/plugins/colorsheme.lua
@@ -2,11 +2,27 @@ return {
 	{
 		"olimorris/onedarkpro.nvim",
 		priority = 1000,
-		config = function()
-			require("onedarkpro").setup({
-                options = { transparency = true }
-			})
-			vim.cmd("colorscheme onedark_vivid")
-		end,
+		-- config = function()
+		-- 	require("onedarkpro").setup({
+		--               options = { transparency = true }
+		-- 	})
+		-- 	vim.cmd("colorscheme onedark_vivid")
+		-- end,
 	},
+    {
+        "rose-pine/neovim",
+        priority = 1000,
+        name = "rose-pine",
+        -- config = function ()
+        --     vim.cmd("colorscheme rose-pine")
+        -- end
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        priority = 1000,
+        name = "kanagawa",
+        -- config = function ()
+        --     vim.cmd(" colorscheme kanagawa")
+        -- end
+    },
 }
