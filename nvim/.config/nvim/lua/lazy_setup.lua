1cbf3112cb79ba3d010060fef8896faebcbb6f84
anatolij1923 <kostyahaustow@gmail.com>
2025-07-07 19:25:10 +0300

    nvim: updates

--- nvim/.config/nvim/lua/lazy_setup.lua
+++ nvim/.config/nvim/lua/lazy_setup.lua
@@ -15,6 +15,7 @@ require("lazy").setup(
     {
         { import = "plugins" },
         { import = "plugins.lsp" },
+        { import = "plugins.themes"},
     },
     {
         checker = {
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,17 +4,20 @@ return {
 	config = function()
 		local lualine = require("lualine")
 		lualine.setup({
-			-- options = {
-			-- 	section_separators = { left = "", right = "" },
-			-- },
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "filename", "branch", "diagnostics", "diff"},
-				lualine_c = {},
-				lualine_x = {"filetype"},
-				lualine_y = {"progress"},
-				lualine_z = {"location"},
+			options = {
+				-- section_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "|", right = "|" },
 			},
+
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "filename", "branch", "diff" },
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = { "diagnostics" },
+                lualine_z = { "location" },
+            },
 		})
 	end,
 }
