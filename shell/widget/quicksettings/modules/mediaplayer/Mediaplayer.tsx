c8d569ca989d131698f86127c56f88428feea20b
anatolij1923 <kostyahaustow@gmail.com>
2025-07-19 16:11:53 +0300

    feat: added progress bar in mediaplayer

--- shell/widget/quicksettings/modules/mediaplayer/Mediaplayer.tsx
+++ shell/widget/quicksettings/modules/mediaplayer/Mediaplayer.tsx
@@ -1,4 +1,4 @@
-import { createBinding, For } from "ags";
+import { createBinding, createComputed, For } from "ags";
 import { Gtk } from "ags/gtk4";
 import AstalApps01 from "gi://AstalApps";
 import Mpris from "gi://AstalMpris";
@@ -12,73 +12,80 @@ export default function Mediaplayer() {
   return (
     <box orientation={Gtk.Orientation.VERTICAL}>
       <For each={players}>
-        {(player) => (
-          <box class="mediaplayer" spacing={16}>
-            <box overflow={Gtk.Overflow.HIDDEN} class="cover-art">
-              <image file={createBinding(player, "coverArt")} pixelSize={96} />
-            </box>
-
-            <box orientation={Gtk.Orientation.VERTICAL}>
-              <box class="info" hexpand>
-                <box orientation={Gtk.Orientation.VERTICAL} spacing={2}>
-                  <label
-                    ellipsize={Pango.EllipsizeMode.END}
-                    maxWidthChars={20}
-                    xalign={0}
-                    label={createBinding(player, "title")}
-                    class="track"
-                  />
-                  <label
-                    xalign={0}
-                    maxWidthChars={15}
-                    ellipsize={Pango.EllipsizeMode.END}
-                    label={createBinding(player, "artist")}
-                    class="artist"
-                  />
-                </box>
-
-                <box hexpand></box>
-                <box>
-                  {(() => {
-                    const [app] = apps.exact_query(player.entry);
-                    return (
-                      <image
-                        visible={!!app.iconName}
-                        iconName={app?.iconName}
-                        pixelSize={32}
-                      />
-                    );
-                  })()}
-                </box>
-              </box>
-
-              <box>
-                <Gtk.ProgressBar
-                  fraction={createBinding(player, "position")}
-                  hexpand
+        {(player) => {
+          const position = createBinding(player, "position");
+          const length = createBinding(player, "length");
+          const progress = createComputed([position, length], (pos, len) => {
+            return pos / len;
+          });
+          return (
+            <box class="mediaplayer" spacing={16}>
+              <box overflow={Gtk.Overflow.HIDDEN} class="cover-art">
+                <image
+                  file={createBinding(player, "coverArt")}
+                  pixelSize={96}
                 />
               </box>
 
-              <box class="buttons" halign={Gtk.Align.CENTER} spacing={16}>
-                <button onClicked={() => player.previous()}>
-                  <label label="skip_previous" class="material-icon" />
-                </button>
-                <button onClicked={() => player.play_pause()}>
-                  <label
-                    label={createBinding(
-                      player,
-                      "playbackStatus"
-                    )((v) => (v ? "play_arrow" : "pause"))}
-                    class="material-icon"
-                  />
-                </button>
-                <button onClicked={() => player.next()}>
-                  <label label="skip_next" class="material-icon" />
-                </button>
+              <box orientation={Gtk.Orientation.VERTICAL}>
+                <box class="info" hexpand>
+                  <box orientation={Gtk.Orientation.VERTICAL} spacing={2}>
+                    <label
+                      ellipsize={Pango.EllipsizeMode.END}
+                      maxWidthChars={20}
+                      xalign={0}
+                      label={createBinding(player, "title")}
+                      class="track"
+                    />
+                    <label
+                      xalign={0}
+                      maxWidthChars={15}
+                      ellipsize={Pango.EllipsizeMode.END}
+                      label={createBinding(player, "artist")}
+                      class="artist"
+                    />
+                  </box>
+
+                  <box hexpand></box>
+                  <box>
+                    {(() => {
+                      const [app] = apps.exact_query(player.entry);
+                      return (
+                        <image
+                          visible={!!app.iconName}
+                          iconName={app?.iconName}
+                          pixelSize={32}
+                        />
+                      );
+                    })()}
+                  </box>
+                </box>
+
+                <box>
+                  <Gtk.ProgressBar fraction={progress} hexpand />
+                </box>
+
+                <box class="buttons" halign={Gtk.Align.CENTER} spacing={16}>
+                  <button onClicked={() => player.previous()}>
+                    <label label="skip_previous" class="material-icon" />
+                  </button>
+                  <button onClicked={() => player.play_pause()}>
+                    <label
+                      label={createBinding(
+                        player,
+                        "playbackStatus"
+                      )((v) => (v ? "play_arrow" : "pause"))}
+                      class="material-icon"
+                    />
+                  </button>
+                  <button onClicked={() => player.next()}>
+                    <label label="skip_next" class="material-icon" />
+                  </button>
+                </box>
               </box>
             </box>
-          </box>
-        )}
+          );
+        }}
       </For>
     </box>
   );
