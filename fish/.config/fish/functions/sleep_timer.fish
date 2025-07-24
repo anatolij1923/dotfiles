ffee58e1f12418b5a91f61a07e6814ddd5b5eb4a
anatolij1923 <kostyahaustow@gmail.com>
2025-07-24 23:22:22 +0300

    fish: add a `sleep_timer` function to Fish shell for convenience.

---
+++ fish/.config/fish/functions/sleep_timer.fish
@@ -0,0 +1,9 @@
+function sleep_timer
+    if test -z "$argv[1]"
+        echo "Usage: sleep_timer <time>"
+        echo "Example: sleep_timer 1h"
+        return 1
+    end
+    echo "Pausing media and suspend in $argv[1]..."
+    sleep $argv[1] && playerctl -a pause && systemctl suspend -f
+end
