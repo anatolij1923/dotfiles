dd3d3bcf1ee7fbb59a1e1e1f735e5f467fc167ef
anatolij1923 <kostyahaustow@gmail.com>
2025-08-01 13:03:45 +0300

    refactor: moved CommonButton to SysButton component

--- shell/widget/bar/Bar.tsx
+++ shell/widget/bar/Bar.tsx
@@ -1,43 +1,12 @@
 import { Astal, Gdk, Gtk } from "ags/gtk4";
 import app from "ags/gtk4/app";
-import Clock from "./modules/clock/Clock";
-import KbLayout from "./modules/kblayout/KbLayout";
-import FocusedClient from "./modules/focusedclient/FocusedClient";
-import Battery from "./modules/battery/Battery";
-import Tray from "./modules/tray/Tray";
 import Workspaces from "./modules/workspaces/Workspaces";
-import BluetoothWidget from "./modules/bluetoothWidget/bluetoothWidget";
-import Wifi from "./modules/wifi/Wifi";
-import NotificationWidget from "./modules/notificationWidget/NotifiicationWidget";
-import { onCleanup } from "ags";
-
-function CommonButton() {
-  return (
-    <box
-      class="common-button"
-      $={(self) => {
-        const appconnect = app.connect("window-toggled", (_, win) => {
-          if (win.name !== "quicksettings") return;
-          const visible = win.visible;
-          self[visible ? "add_css_class" : "remove_css_class"]("active");
-        });
-        onCleanup(() => app.disconnect(appconnect));
-      }}
-    >
-      <button
-        onClicked={() => {
-          app.toggle_window("quicksettings");
-        }}
-      >
-        <box spacing={16}>
-          <NotificationWidget />
-          <Wifi />
-          <BluetoothWidget />
-        </box>
-      </button>
-    </box>
-  );
-}
+import FocusedClient from "./modules/focusedclient/FocusedClient";
+import Clock from "./modules/clock/Clock";
+import Tray from "./modules/tray/Tray";
+import KbLayout from "./modules/kblayout/KbLayout";
+import SysButton from "./modules/sysbutton/SysButton";
+import Battery from "./modules/battery/Battery";
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
   const { TOP, RIGHT, LEFT } = Astal.WindowAnchor;
@@ -62,7 +31,7 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
         <box $type="end" class="right-side" spacing={8}>
           <Tray />
           <KbLayout />
-          <CommonButton />
+          <SysButton />
           <Battery />
         </box>
       </centerbox>
