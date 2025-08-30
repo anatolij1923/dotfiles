06b4bc52abccdaf3c3f18345d23350a3f8bf765c
anatolij1923 <kostyahaustow@gmail.com>
2025-08-30 13:40:46 +0300

    nvim: cleaned nvim-cmp deps

--- nvim/.config/nvim/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -1,57 +1,56 @@
 return {
-	"williamboman/mason.nvim",
-	-- lazy = false,
+    "williamboman/mason.nvim",
+    -- lazy = false,
     event = "VeryLazy",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		"hrsh7th/cmp-nvim-lsp",
-		"neovim/nvim-lspconfig",
-	},
-	config = function()
-		-- import mason and mason_lspconfig
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
-		local mason_tool_installer = require("mason-tool-installer")
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        "neovim/nvim-lspconfig",
+    },
+    config = function()
+        -- import mason and mason_lspconfig
+        local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
+        local mason_tool_installer = require("mason-tool-installer")
 
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+        -- enable mason and configure icons
+        mason.setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        })
 
-		mason_lspconfig.setup({
-			automatic_enable = false,
-			-- servers for mason to install
-			ensure_installed = {
-				"lua_ls",
-				"cssls",
-				"clangd",
-				"html",
+        mason_lspconfig.setup({
+            automatic_enable = false,
+            -- servers for mason to install
+            ensure_installed = {
+                "lua_ls",
+                "cssls",
+                "clangd",
+                "html",
                 "ts_ls",
-				"eslint",
-				"emmet_ls",
-				"emmet_language_server",
-			},
-		})
+                "eslint",
+                "emmet_ls",
+                "emmet_language_server",
+            },
+        })
 
-		mason_tool_installer.setup({
-			ensure_installed = {
+        mason_tool_installer.setup({
+            ensure_installed = {
                 "prettier",
-				"stylua",
-				"clang-format",
+                "stylua",
+                "clang-format",
                 "eslint_d",
-				"black",
-				"denols",
-				"pylint",
-				"rust-analyzer",
+                "black",
+                "denols",
+                "pylint",
+                "rust-analyzer",
                 "qmlls",
-			},
-		})
-	end,
+            },
+        })
+    end,
 }
