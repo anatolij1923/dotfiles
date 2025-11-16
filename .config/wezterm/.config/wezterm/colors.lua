0fc082da5612aa1a00fd0900f079fa6a12f7d8cd
anatolij1923 <kostyahaustow@gmail.com>
2025-11-16 17:15:13 +0300

    wezterm: added wezterm config

---
+++ .config/wezterm/.config/wezterm/colors.lua
@@ -0,0 +1,64 @@
+return {
+	-- Основные
+	foreground = "#f1dedc",
+	background = "#1a1110",
+
+	-- Курсор
+	cursor_bg = "#ffb3ad",
+	cursor_fg = "#1a1110",
+	cursor_border = "#ffb3ad",
+
+	-- Выделение
+	selection_bg = "#73332f",
+	selection_fg = "#ffdad6",
+
+	-- Скроллбар
+	scrollbar_thumb = "#a08c8a",
+
+	-- Сплиттеры
+	split = "#534342",
+
+	-- Статус-бар
+	tab_bar = {
+		background = "#140c0b",
+		active_tab = {
+			bg_color = "#ffb3ad",
+			fg_color = "#571e1b",
+		},
+		inactive_tab = {
+			bg_color = "#231918",
+			fg_color = "#d8c2bf",
+		},
+		inactive_tab_hover = {
+			bg_color = "#73332f",
+			fg_color = "#ffdad6",
+			italic = true,
+		},
+		new_tab = {
+			bg_color = "#140c0b",
+			fg_color = "#f1dedc",
+		},
+		new_tab_hover = {
+			bg_color = "#ffb3ad",
+			fg_color = "#571e1b",
+		},
+	},
+
+	-- Панель с подсказками клавиш
+	-- quick_select_label_bg = "#73332f",
+	-- quick_select_label_fg = "#ffdad6",
+	-- quick_select_match_bg = "#5d3f3d",
+	-- quick_select_match_fg = "#ffdad6",
+
+	-- Панель поиска
+	-- search = {
+	-- 	matches = {
+	-- 		bg_color = "#5d3f3d",
+	-- 		fg_color = "#ffdad6",
+	-- 	},
+	-- 	focused_match = {
+	-- 		bg_color = "#584419",
+	-- 		fg_color = "#ffdea6",
+	-- 	},
+	-- },
+}
---
+++ .config/wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,28 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("JetBrains Mono", { weight = "Regular" })
+config.font_size = 16
+
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.tab_max_width = 30
+
+config.color_scheme = "Kanagawa Dragon (Gogh)"
+
+local colors = require("colors")
+config.colors = colors
+
+config.enable_scroll_bar = true
+
+config.window_padding = {
+	top = 0,
+	bottom = 0,
+	left = 0,
+	right = 0,
+}
+
+config.window_background_opacity = 0.8
+
+return config
