61dbfbe8c27d63ad9e7800f8e071d4317d257141
anatolij1923 <kostyahaustow@gmail.com>
2025-11-19 16:05:40 +0300

    feat(nvim): added lsp name in lualine

--- .config/nvim/.config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,89 +1,96 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-web-devicons" },
-    config = function()
-        local lualine = require("lualine")
-        local devicons = require("nvim-web-devicons")
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-web-devicons" },
+	config = function()
+		local lualine = require("lualine")
+		local devicons = require("nvim-web-devicons")
 
-        local function curr_dir()
-            local dir_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-            return "  " .. dir_name
-        end
+		local function curr_dir()
+			local dir_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+			return "  " .. dir_name
+		end
 
-        local function file_with_icon()
-            local filename = vim.fn.expand("%:t")
-            local icon, icon_color = devicons.get_icon_color(filename, vim.bo.filetype, { default = true })
-            if filename == "" then
-                filename = "[No Name]"
-                icon = ""
-                icon_color = nil
-            end
-            local status = ""
-            if vim.bo.modified then
-                status = " ●" -- изменённый файл
-            elseif vim.bo.readonly then
-                status = " " -- только для чтения
-            end
+		local function file_with_icon()
+			local filename = vim.fn.expand("%:t")
+			local extension = vim.fn.expand("%:e")
+			local icon, icon_color = devicons.get_icon_color(filename, extension, { default = true })
 
-            return icon .. " " .. filename .. status, icon_color
-        end
+			if filename == "" then
+				filename = "[No Name]"
+				icon = ""
+				icon_color = "#aaaaaa"
+			end
 
-        local function hl_color(group)
-            local ok, hl = pcall(vim.api.nvim_get_hl_by_name, group, true)
-            if not ok then return nil end
-            return hl.foreground and string.format("#%06x", hl.foreground) or nil
-        end
+			local status = ""
+			if vim.bo.modified then
+				status = " ●"
+			elseif vim.bo.readonly then
+				status = " "
+			end
 
-        lualine.setup({
-            options = {
-                -- section_separators = { left = "", right = "" },
-                section_separators = { left = "", right = "" },
-                component_separators = { left = "", right = "" },
-                theme = "auto"
-            },
+			return icon .. " " .. filename .. status, { fg = icon_color }
+		end
 
-            sections = {
-                lualine_a = { {
-                    "mode",
-                    color = { gui = "bold" }
-                } },
-                lualine_b = {
-                    {
-                        "branch",
-                        icon = ""
-                    },
-                    {
-                        "diff",
-                        symbols = { added = " ", modified = " ", removed = " " },
-                    },
+		local function lsp_name()
+			local msg = "No active LSP"
+			local clients = vim.lsp.get_clients({ bufnr = 0 })
 
+			if #clients == 0 then
+				return msg
+			end
 
-                },
-                lualine_c = {
-                    {
-                        file_with_icon,
-                    },
-                },
-                lualine_x = {
-                },
-                lualine_y = {
-                    {
-                        "diagnostics",
-                        symbols = {
-                            error = " ",
-                            warn = " ",
-                            info = " ",
-                            hint = " ",
-                        },
-                    },
-                    { curr_dir },
+			local client_names = {}
 
+			for _, client in ipairs(clients) do
+				table.insert(client_names, client.name)
+			end
 
-                },
-                lualine_z = {
+			if #client_names == 0 then
+				return msg
+			end
 
-                },
-            },
-        })
-    end,
+			return table.concat(client_names, ", ")
+		end
+
+		lualine.setup({
+			options = {
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+				theme = "auto",
+				globalstatus = true,
+			},
+
+			sections = {
+				lualine_a = {
+					{
+						"mode",
+						color = { gui = "bold" },
+					},
+				},
+				lualine_b = {
+					{ "branch", icon = "" },
+					{ "diff", symbols = { added = " ", modified = " ", removed = " " } },
+				},
+				lualine_c = {
+					{
+						file_with_icon,
+					},
+				},
+				lualine_x = {
+					{
+						lsp_name,
+						icon = " LSP:",
+					},
+				},
+				lualine_y = {
+					{
+						"diagnostics",
+						symbols = { error = " ", warn = " ", info = " ", hint = " " },
+					},
+					{ curr_dir },
+				},
+				lualine_z = {},
+			},
+		})
+	end,
 }
