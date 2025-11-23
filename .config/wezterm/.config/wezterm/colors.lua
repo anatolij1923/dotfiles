888b8e4479614bdcd2cbfcf4d3e1bd99191858fe
anatolij1923 <kostyahaustow@gmail.com>
2025-11-23 23:46:23 +0300

    chore: update gitignore

--- .config/wezterm/.config/wezterm/colors.lua
+++ .config/wezterm/.config/wezterm/colors.lua
@@ -1,64 +1,64 @@
 return {
 	-- Основные
-	foreground = "#e0e4db",
-	background = "#10140f",
+	foreground = "#e4e1e9",
+	background = "#131318",
 
 	-- Курсор
-	cursor_bg = "#a1d39a",
-	cursor_fg = "#10140f",
-	cursor_border = "#a1d39a",
+	cursor_bg = "#c1c1ff",
+	cursor_fg = "#131318",
+	cursor_border = "#c1c1ff",
 
 	-- Выделение
-	selection_bg = "#245024",
-	selection_fg = "#bcf0b4",
+	selection_bg = "#404178",
+	selection_fg = "#e1dfff",
 
 	-- Скроллбар
-	scrollbar_thumb = "#8c9388",
+	scrollbar_thumb = "#918f9a",
 
 	-- Сплиттеры
-	split = "#424940",
+	split = "#47464f",
 
 	-- Статус-бар
 	tab_bar = {
-		background = "#0b0f0a",
+		background = "#0e0e13",
 		active_tab = {
-			bg_color = "#a1d39a",
-			fg_color = "#0a390f",
+			bg_color = "#c1c1ff",
+			fg_color = "#292a60",
 		},
 		inactive_tab = {
-			bg_color = "#191d17",
-			fg_color = "#c2c9bd",
+			bg_color = "#1b1b21",
+			fg_color = "#c8c5d0",
 		},
 		inactive_tab_hover = {
-			bg_color = "#245024",
-			fg_color = "#bcf0b4",
+			bg_color = "#404178",
+			fg_color = "#e1dfff",
 			italic = true,
 		},
 		new_tab = {
-			bg_color = "#0b0f0a",
-			fg_color = "#e0e4db",
+			bg_color = "#0e0e13",
+			fg_color = "#e4e1e9",
 		},
 		new_tab_hover = {
-			bg_color = "#a1d39a",
-			fg_color = "#0a390f",
+			bg_color = "#c1c1ff",
+			fg_color = "#292a60",
 		},
 	},
 
 	-- Панель с подсказками клавиш
-	-- quick_select_label_bg = "#245024",
-	-- quick_select_label_fg = "#bcf0b4",
-	-- quick_select_match_bg = "#3b4b38",
-	-- quick_select_match_fg = "#d5e8ce",
+	-- quick_select_label_bg = "#404178",
+	-- quick_select_label_fg = "#e1dfff",
+	-- quick_select_match_bg = "#454559",
+	-- quick_select_match_fg = "#e2e0f9",
 
 	-- Панель поиска
 	-- search = {
 	-- 	matches = {
-	-- 		bg_color = "#3b4b38",
-	-- 		fg_color = "#d5e8ce",
+	-- 		bg_color = "#454559",
+	-- 		fg_color = "#e2e0f9",
 	-- 	},
 	-- 	focused_match = {
-	-- 		bg_color = "#1f4d52",
-	-- 		fg_color = "#bcebf0",
+	-- 		bg_color = "#5f3c51",
+	-- 		fg_color = "#ffd8ec",
 	-- 	},
 	-- },
 }
