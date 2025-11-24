34919db88d2c44364e2dc1b6038260288d399f18
anatolij1923 <kostyahaustow@gmail.com>
2025-11-24 21:14:53 +0300

    feat(nvim): add keymap for esc ("jk")

--- .config/nvim/.config/nvim/lua/core/keymaps.lua
+++ .config/nvim/.config/nvim/lua/core/keymaps.lua
@@ -3,7 +3,7 @@ local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- Sync clipboard
-vim.api.nvim_set_keymap('v', '<C-c>', '"+y', opts)
+vim.api.nvim_set_keymap("v", "<C-c>", '"+y', opts)
 
 map("n", ";", ":")
 
@@ -45,17 +45,18 @@ vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", opts)
 
 -- Close tab
 map("n", "<leader>q", "<Cmd>bdelete<CR>", {
-    noremap = true,
-    silent = true,
-    desc = "Buffer: Close current"
+	noremap = true,
+	silent = true,
+	desc = "Buffer: Close current",
 })
 
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+vim.keymap.set("i", "jk", "<Esc>")
