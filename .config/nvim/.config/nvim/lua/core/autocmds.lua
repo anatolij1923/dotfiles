81f09cf20c8101db40c890399e850a691619a96e
anatolij1923 <kostyahaustow@gmail.com>
2026-01-05 16:38:05 +0300

    feat: add autocmd for returning to last edit position

--- .config/nvim/.config/nvim/lua/core/autocmds.lua
+++ .config/nvim/.config/nvim/lua/core/autocmds.lua
@@ -2,6 +2,19 @@ local function augroup(name)
     return vim.api.nvim_create_augroup("user_" .. name, { clear = true })
 end
 
+-- Return to last edit position when opening a file
+vim.api.nvim_create_autocmd("BufReadPost", {
+    group = augroup("last_loc"),
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
+})
+
+
 -- Set indentation for specific filetypes
 vim.api.nvim_create_autocmd("FileType", {
     group = augroup("set_indent"),
