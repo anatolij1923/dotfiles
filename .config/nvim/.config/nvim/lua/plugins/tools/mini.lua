0b45275778c62bbdbe8544f9979792919e758aa8
anatolij1923 <kostyahaustow@gmail.com>
2026-01-10 19:24:39 +0300

    nvim: remove mini-files

--- .config/nvim/.config/nvim/lua/plugins/tools/mini.lua
+++ .config/nvim/.config/nvim/lua/plugins/tools/mini.lua
@@ -1,33 +1,19 @@
 return {
-	"nvim-mini/mini.nvim",
-	version = false,
-	config = function()
-		-- mini.files
-		require("mini.files").setup({
-			mappings = {
-				close = "q",
-				go_in = "l",
-				go_out = "h",
-				synchronize = "=",
-			},
-			windows = {
-				preview = true,
-				width_focus = 30,
-			},
-		})
+    "nvim-mini/mini.nvim",
+    version = false,
+    config = function()
+        vim.keymap.set("n", "-", function()
+            require("mini.files").open(vim.api.nvim_buf_get_name(0))
+        end)
 
-		vim.keymap.set("n", "-", function()
-			require("mini.files").open(vim.api.nvim_buf_get_name(0))
-		end)
+        -- mini.icons
+        local icons = require("mini.icons")
+        icons.setup()
+        icons.mock_nvim_web_devicons()
 
-		-- mini.icons
-		local icons = require("mini.icons")
-		icons.setup()
-		icons.mock_nvim_web_devicons()
+        -- mini.pairs
+        require("mini.pairs").setup()
 
-		-- mini.pairs
-		require("mini.pairs").setup()
-
-		-- mini.notify
-	end,
+        -- mini.notify
+    end,
 }
