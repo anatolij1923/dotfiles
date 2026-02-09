25ab1909ab4f61d95eed469c8b7e682fb040b4cd
anatolij1923 <kostyahaustow@gmail.com>
2026-02-09 18:13:49 +0300

    nvim: formatting

--- .config/nvim/.config/nvim/lua/plugins/editor/nvimtree.lua
+++ .config/nvim/.config/nvim/lua/plugins/editor/nvimtree.lua
@@ -1,36 +1,36 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	version = "*",
-	dependencies = { "nvim-mini/mini.nvim" },
-	keys = {
-		{ "\\", "<cmd>NvimTreeToggle<cr>", desc = "Toggle file explorer" },
-	},
-	config = function()
-		require("nvim-tree").setup({
-			disable_netrw = true,
-			hijack_cursor = true,
-			sync_root_with_cwd = true,
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = { "nvim-mini/mini.nvim" },
+    keys = {
+        { "\\", "<cmd>NvimTreeToggle<cr>", desc = "Toggle file explorer" },
+    },
+    config = function()
+        require("nvim-tree").setup({
+            disable_netrw = true,
+            hijack_cursor = true,
+            sync_root_with_cwd = true,
 
-			view = {
-				width = 30,
-			},
-			renderer = {
-				icons = {
-					-- Icons will be automatically provided by mini.icons mock
-					show = {
-						file = true,
-						folder = true,
-						folder_arrow = true,
-						git = true,
-					},
-					-- glyphs = {
-					-- 	folder = {
-					-- 		arrow_closed = "󰅂",
-					-- 		arrow_open = "󰅀",
-					-- 	},
-					-- },
-				},
-			},
-		})
-	end,
+            view = {
+                width = 30,
+            },
+            renderer = {
+                icons = {
+                    -- Icons will be automatically provided by mini.icons mock
+                    show = {
+                        file = true,
+                        folder = true,
+                        folder_arrow = true,
+                        git = true,
+                    },
+                    -- glyphs = {
+                    -- 	folder = {
+                    -- 		arrow_closed = "󰅂",
+                    -- 		arrow_open = "󰅀",
+                    -- 	},
+                    -- },
+                },
+            },
+        })
+    end,
 }
--- .config/nvim/.config/nvim/lua/plugins/lsp/conform.lua
+++ .config/nvim/.config/nvim/lua/plugins/lsp/conform.lua
@@ -1,81 +1,81 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
 
-		local custom_formatters = {
-			["clang-format"] = {
-				args = {
-					"--assume-filename",
-					"$FILENAME",
-					"--style",
-					"{BasedOnStyle: LLVM, IndentWidth: 4, TabWidth: 4, UseTab: Never}",
-				},
-			},
+        local custom_formatters = {
+            ["clang-format"] = {
+                args = {
+                    "--assume-filename",
+                    "$FILENAME",
+                    "--style",
+                    "{BasedOnStyle: LLVM, IndentWidth: 4, TabWidth: 4, UseTab: Never}",
+                },
+            },
 
-			prettier = {
-				args = {
-					"--stdin-filepath",
-					"$FILENAME",
-					"--tab-width",
-					"2",
-					"--use-tabs",
-					"false",
-				},
-			},
-		}
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				svelte = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				-- markdown = { "prettier" },
-				graphql = { "prettier" },
-				liquid = { "prettier" },
-				lua = { "stylua" },
-				python = { "black" },
-				markdown = { "prettier" },
-				["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
-				c = { "clang-format" },
-				cpp = { "clang-format" },
-				qml = { "qmlls" },
-				nix = { "nixfmt" },
-				bash = { "shfmt" },
-				sh = { "shfmt" },
-			},
-			formatters = custom_formatters,
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
-		})
+            prettier = {
+                args = {
+                    "--stdin-filepath",
+                    "$FILENAME",
+                    "--tab-width",
+                    "2",
+                    "--use-tabs",
+                    "false",
+                },
+            },
+        }
+        conform.setup({
+            formatters_by_ft = {
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+                svelte = { "prettier" },
+                css = { "prettier" },
+                html = { "prettier" },
+                json = { "prettier" },
+                yaml = { "prettier" },
+                -- markdown = { "prettier" },
+                graphql = { "prettier" },
+                liquid = { "prettier" },
+                lua = { "stylua" },
+                python = { "black" },
+                markdown = { "prettier" },
+                ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+                c = { "clang-format" },
+                cpp = { "clang-format" },
+                qml = { "qmlls" },
+                nix = { "nixfmt" },
+                bash = { "shfmt" },
+                sh = { "shfmt" },
+            },
+            formatters = custom_formatters,
+            -- format_on_save = {
+            -- 	lsp_fallback = true,
+            -- 	async = false,
+            -- 	timeout_ms = 1000,
+            -- },
+        })
 
-		-- Configure individual formatters
-		-- conform.formatters.prettier = {
-		--     args = {
-		--         "--stdin-filepath",
-		--         "$FILENAME",
-		--         "--tab-width",
-		--         "2",
-		--         "--use-tabs",
-		--         "false",
-		--     },
-		-- }
+        -- Configure individual formatters
+        -- conform.formatters.prettier = {
+        --     args = {
+        --         "--stdin-filepath",
+        --         "$FILENAME",
+        --         "--tab-width",
+        --         "2",
+        --         "--use-tabs",
+        --         "false",
+        --     },
+        -- }
 
-		vim.keymap.set({ "n", "v" }, "f", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format whole file or range (in visual mode)" })
-	end,
+        vim.keymap.set({ "n", "v" }, "f", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            })
+        end, { desc = "Format whole file or range (in visual mode)" })
+    end,
 }
