bcde95f29669405817b870fe8f2e844fbbd4e21e
anatolij1923 <kostyahaustow@gmail.com>
2026-02-22 21:00:00 +0300

    nvim: use prettier for jsonc

--- .config/nvim/.config/nvim/lua/plugins/lsp/conform.lua
+++ .config/nvim/.config/nvim/lua/plugins/lsp/conform.lua
@@ -35,6 +35,7 @@ return {
                 css = { "prettier" },
                 html = { "prettier" },
                 json = { "prettier" },
+                jsonc = { "prettier" },
                 yaml = { "prettier" },
                 -- markdown = { "prettier" },
                 graphql = { "prettier" },
